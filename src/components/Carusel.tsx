1a0293f1207037f2167f6ef96708716165fe8ce8
Andriikryl <andreyk557@gmail.com>
2023-07-16 18:55:27 +0300

    add carusel component

---
+++ src/components/Carusel.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "../App.css";
+
+export default function Carusel({ images }) {
+  const [curentIndex, setCurentIndex] = useState(0);
+
+  const handelNext = () => {
+    setCurentIndex((prevIndex) => {
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1;
+    });
+  };
+  const handelPrevios = () => {
+    setCurentIndex((prevIndex) => {
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1;
+    });
+  };
+  const handleDotClick = (index) => {
+    setCurentIndex(index);
+  };
+  return (
+    <div className="carousel">
+      <img key={curentIndex} src={images[curentIndex]} />
+      <div className="slide_direction">
+        <div className="left" onClick={handelPrevios}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 96 960 960"
+            width="20"
+          >
+            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+          </svg>
+        </div>
+        <div className="right" onClick={handelNext}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="20"
+            viewBox="0 96 960 960"
+            width="20"
+          >
+            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+          </svg>
+        </div>
+      </div>
+      <div className="indicator">
+        {images.map((_, index) => (
+          <div
+            key={index}
+            className={`dot ${curentIndex === index ? "active" : ""}`}
+            onClick={() => handleDotClick(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+}
